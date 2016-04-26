2961043418dba1ec6faacbcc6d2ec19cd8b332e3
Adrian Stainforth <adrian@morsedigital.com>
2016-04-26 16:59:51 +0100

    add error messaging on watchify

--- plugins/base.js
+++ plugins/base.js
@@ -1,28 +1,32 @@
-var _          = require("lodash")
- , browserify  = require("browserify")
- , config      = require("./config")
- , concat      = require("concat-stream")
- , es          = require("event-stream")
- , fs          = require("fs")
- , path        = require("path")
- , Uglify      = require("./uglify_bundle")
- , utils       = require("./utils")
- , watcher     = require("./watch_bundle")
+var _          = require('lodash')
+ , browserify  = require('browserify');
+ // , concat      = require('concat-stream')
+ // , es          = require('event-stream')
+ // , fs          = require('fs')
+ // , path        = require('path');
 
+var config     = require('./config')
+ , Uglify      = require('./uglify_bundle')
+ , utils       = require('./utils')
+ , watcher     = require('./watch_bundle');
 
-var inPaths  = utils.fileIn(config.get("input"))
-var outPaths = utils.fileRegExp(config.get("ext"), config.get("output"));
+var inPaths  = utils.fileIn(config.get('input'));
+// var outPaths = utils.fileRegExp(config.get('ext'), config.get('output'));
 
 function bundleShare(b, fileStream) {
   b.bundle()
-   .on("data", function(err){
-    if(err.message)
-      console.log(err.toString());
-      // this.close();
+ .on('data'
+  , function(err){
+    if (err.message){
+      console.warn(err.toString());
+    }
+  })
+   .on('error'
+    , function(err){
+      console.error(err.toString());
     })
    .pipe(fileStream());
 }
-
 
 module.exports = function(files, minify){
   minify = minify || false;
@@ -31,56 +35,61 @@ module.exports = function(files, minify){
 
   var inputs = _.map(files, function(f){
     return inPaths(f);
-  })
+  });
 
   var output = utils.setOutpaths(files, minify);
   var uglify = (minify) ? Uglify(files) :  null;
 
-  var bundle = browserify({entries:inputs, extensions:config.get("ext"), debug:true, cache: {}, packageCache: {}})
-  if(!_.isEmpty(config.get("ignore"))){
-    _.forEach(config.get("ignore"), function(ig){
-      bundle.ignore(ig)
-    })
+  var bundle = browserify({
+    entries: inputs
+    , extensions: config.get('ext')
+    , debug: true
+    , cache: {}
+    , packageCache: {}
+  });
+  if (!_.isEmpty(config.get('ignore'))){
+    _.forEach(config.get('ignore'), function(ig){
+      bundle.ignore(ig);
+    });
   }
   var obj = {
-    addTransforms:function(){
-      _.forEach(config.get("transforms"), function(t){
-        if(_.isArray(t)){
+    addTransforms: function(){
+      _.forEach(config.get('transforms'), function(t){
+        if (_.isArray(t)){
           bundle.transform(t[0], t[1]);
         } else {
-          bundle.transform(t)
+          bundle.transform(t);
+        }
+      });
+
+      return obj;
+    }
+    , build: function(factor){
+        if (_.isUndefined(fileStream)) return;
+
+        if (factor){
+          bundle.plugin('factor-bundle', {outputs: output()});
         }
 
-      })
+        bundleShare(bundle, fileStream);
+      }
+    , getBundle: function(){return bundle;}
+    , setFileStream: function(name, server){
+        if (_.isUndefined(fileStream)){
+          fileStream = utils.writeFile(name, uglify, server);
+        }
 
-      return obj;
-    }
-    , build:function(factor){
-      if(_.isUndefined(fileStream)) return;
-      if(factor){
-        bundle.plugin("factor-bundle", { outputs:output() });
+        return obj;
+      }
+    , watch: function(factor){
+        if (_.isUndefined(fileStream)) return;
+        var w = watcher(bundle, bundleShare);
+        if (factor) w.addFactorBundle(output);
+        w.startWatching(fileStream);
+        bundleShare(bundle, fileStream);
       }
 
-      bundleShare(bundle, fileStream);
-    }
-    , getBundle:function(){return bundle;}
-    , setFileStream:function(name, server){
-      if(_.isUndefined(fileStream)){
-        fileStream = utils.writeFile(name, uglify, server);
-      }
-
-      return obj;
-    }
-    , watch:function(factor){
-      if(_.isUndefined(fileStream)) return;
-      var w = watcher(bundle, bundleShare);
-      if(factor) w.addFactorBundle(output)
-      w.startWatching(fileStream);
-      bundleShare(bundle, fileStream);
-    }
-
-  }
+  };
 
   return obj;
-
-}
+};
--- plugins/watch_bundle.js
+++ plugins/watch_bundle.js
@@ -1,19 +1,24 @@
-var watchify    = require("watchify");
+
+var watchify = require('watchify');
 
 module.exports = function(b, bundleShare){
   var w = watchify(b);
 
   var obj = {
-    addFactorBundle:function(output){
-      w.plugin("factor-bundle", { outputs:output() });
+    addFactorBundle: function(output){
+      w.plugin('factor-bundle', {outputs: output()});
       return obj;
     }
-    , startWatching:function(fileStream){
-       w.on("update", function(d){
-        bundleShare(w, fileStream);
-      });
-    }
-  }
+    , startWatching: function(fileStream){
+        w.on('update', function(d){
+          console.warn(d);
+          bundleShare(w, fileStream);
+        })
+        .on('log', function(err){
+          console.warn(err.toString());
+        });
+      }
+  };
 
   return obj;
-}
+};
